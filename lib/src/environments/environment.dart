c96803c466ae6aafd0e21e2225179cc6fe098308
Mariodev27 <mariosantisteban23@gmail.com>
2024-04-30 18:54:29 -0500

    ip_change

--- lib/src/environments/environment.dart
+++ lib/src/environments/environment.dart
@@ -1,7 +1,7 @@
-//Se esta utilizando el IP MAQUINA para la llamada de las rutas 
+//Se esta utilizando el IP MAQUINA para la llamada de las rutas
 // static const String API_URL = String.fromEnvironment('URL_IP', defaultValue: 'http://localhost:3000');
 
 class Environment {
-  static const String API_URL = "http://192.168.18.2:3000/" ;
-  static const String API_URL_OLD = "192.168.18.2:3000";
+  static const String API_URL = "http://192.168.18.2:3000/";
+  static const String API_URL_OLD = "10.200.174.255:3000";
 }
